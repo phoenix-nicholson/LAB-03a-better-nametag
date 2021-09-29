e254fe829e93843128d3312f6e6ed000d16ca15b
Phoenix Nicholson <phoenix.nicholson2008@gmail.com>
2021-09-29 14:40:09 -0700

    Finished Name tag

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const firstName = document.getElementById('first-name');
 const button1 = document.getElementById('green');
 const button2 = document.getElementById('pink');
 const button3 = document.getElementById('purple');
+const button4 = document.getElementById('he');
+const button5 = document.getElementById('she');
+const pronoun = document.getElementById('pronoun');
+const button6 = document.getElementById('they');
 // initialize global state
 
 // set event listeners 
@@ -33,7 +37,18 @@ button3.addEventListener('click', ()=> {
   document.getElementById('header').style.backgroundColor = 'purple';
   document.getElementById('footer').style.backgroundColor = 'purple';
 });
+button4.addEventListener('click', ()=> {
+  document.getElementById('pronoun').textContent = 'He/Him';
 
+
+});
+button5.addEventListener('click', ()=> {
+  document.getElementById('pronoun').textContent = 'She/Her';
+ 
+});
+button6.addEventListener('click', ()=> {
+  document.getElementById('pronoun').textContent = 'They/Them';
+});
 
   // get user input
   // use user input to update state 
